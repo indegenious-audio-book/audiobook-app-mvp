1be5988026a7daeed04a75d8f6321eceef146e14
Ahitosh <ahitoshbhattacharjee@gmail.com>
2020-04-15 16:24:09 +0530

    modified file org

--- ng-audiobook-app/src/app/app.module.ts
+++ ng-audiobook-app/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular";
-import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
-
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -14,8 +12,7 @@ import { AppComponent } from "./app.component";
     imports: [
         AppRoutingModule,
         NativeScriptModule,
-        NativeScriptUISideDrawerModule,
-        NativeScriptHttpClientModule
+        NativeScriptUISideDrawerModule
     ],
     declarations: [
         AppComponent
