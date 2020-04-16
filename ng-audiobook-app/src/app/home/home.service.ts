88505f05c5b8930807ed512690ba7e95f09e84f6
Ahitosh <ahitoshbhattacharjee@gmail.com>
2020-04-16 18:46:49 +0530

    book list gallery added

--- ng-audiobook-app/src/app/home/home.service.ts
+++ ng-audiobook-app/src/app/home/home.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { BehaviorSubject } from "rxjs";
-import { Book, BookEntity } from "./home.model";
 
 @Injectable({ providedIn: "root" })
 export class HomeService {
